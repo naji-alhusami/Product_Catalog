be1db6bac8fa93b0cc5aa475a4d2e760f6d99163
Alhusami <najihussami@gmail.com>
2025-07-06 21:09:17 +0200

    fixed the brands issues

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["wittedigitalsauat.blob.core.windows.net"],
+    domains: ["wittedigitalsaprod.blob.core.windows.net"],
   },
 };
 
--- src/components/Keys/FiltersSidebar.tsx
+++ src/components/Keys/FiltersSidebar.tsx
@@ -1,5 +1,7 @@
+import { getAllKeys } from "../lib/getAllKeys";
 import { getBoxesClasses } from "../lib/getBoxesClasses";
 import { getBrands } from "../lib/getBrands";
+import { getKeysByBrand } from "../lib/getKeysByBrand";
 import { getValidKeys } from "../lib/getValidKeys";
 import BrandsFilter from "./BrandsFilter";
 import ClassesFilters from "./ClassesFilters";
@@ -7,18 +9,15 @@ import TypesFilters from "./TypesFilters";
 
 export default async function FiltersSidebar() {
   const brands = await getBrands();
-
-  const allKeys = await getValidKeys();
-  const AllClasses = allKeys
-    .map((key) => getBoxesClasses(key.boxSapNumber))
-    .filter((cls): cls is string => cls !== null);
+  const AllKeys = await getAllKeys();
+  console.log(AllKeys);
 
   if (brands) {
     return (
       <div>
         <BrandsFilter brands={brands} />
-        <ClassesFilters AllClasses={AllClasses} />
-        <TypesFilters allKeys={allKeys} />
+        {/* <ClassesFilters AllClasses={AllClasses} />
+        <TypesFilters allKeys={allKeys} /> */}
       </div>
     );
   }
